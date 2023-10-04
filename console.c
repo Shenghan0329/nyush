e4db86d126081a4c1d14f0c5f58c73835dd2628e
Shenghan Cheng <sc8326@nyu.edu>
2023-10-04 19:22:23 -0400

    Debugging and improve functionalities

--- console.c
+++ console.c
@@ -34,7 +34,6 @@ void ctrlC(int sig){
         currentProcess = 0;
     }
     toBreak = 1;
-    printf("\n");
 }
 void ctrlZ(int sig){
     if(sig != -1){
@@ -134,14 +133,14 @@ int isValid(int argc, char ** argv){
 void cd(char* dir,int argNum){
     // Check if too many or too few arguments
     if(argNum != 1){
-        printf("%s",invalidCommand);
+        fprintf(stderr,invalidCommand);
         return;
     }
     // Reference: https://www.geeksforgeeks.org/how-to-check-a-file-or-directory-exists-in-cpp/
     // Check if Directory valid
     struct stat sb;
     if (stat(dir, &sb) != 0){
-        printf("%s",invalidDirectory);
+        fprintf(stderr,invalidDirectory);
         return;
     }
     char s[1000];
@@ -152,15 +151,15 @@ void cd(char* dir,int argNum){
 // BUILDIN JOBS
 void jobs(int argNum){
     if(argNum != 0){
-        printf(invalidCommand);
+        fprintf(stderr,invalidCommand);
         return;
     }
     if(suspendedProcess[0] != 0){
         for(int i = 0; i<processIndex; i++){
-            printf("[%d] ",i+1);
+            printf("[%d]",i+1);
             int j = 0;
             while(suspendedCommandName[i][j] != NULL){
-                printf("%s ",suspendedCommandName[i][j++]);
+                printf(" %s",suspendedCommandName[i][j++]);
             }
             printf("\n");
         }
@@ -171,39 +170,50 @@ void jobs(int argNum){
 // BUILDIN FG
 void fg(int index, int argNum){
     if(argNum != 1){
-        printf(invalidCommand);
+        fprintf(stderr,invalidCommand);
         return;
     }
     if(index<1 || index > processIndex){
-        printf(invalidJob);
+        fprintf(stderr,invalidJob);
         return;
     }
     kill(suspendedProcess[index-1], SIGCONT);
-    int x = 0;
-    while(suspendedCommandName[index-1][x] != NULL){
-        free(suspendedCommandName[index-1][x]);
-        x++;
+    int status;
+    waitpid(suspendedProcess[index-1], &status, WUNTRACED);
+    int currProcess = suspendedProcess[index-1];
+    char** currCommandName = suspendedCommandName[index-1];
+    if(status == 0){
+        int x = 0;
+        while(suspendedCommandName[index-1][x] != NULL){
+            free(suspendedCommandName[index-1][x]);
+            x++;
+        }
+        free(suspendedCommandName[index-1]);
     }
-    free(suspendedCommandName[index-1]);
     for(int i = index-1; i<processIndex-1; i++){
         suspendedCommandName[i] = suspendedCommandName[i+1];
         suspendedProcess[i] = suspendedProcess[i+1];
     }
-    suspendedCommandName[processIndex-1] = NULL;
-    suspendedProcess[processIndex-1] = 0;
-    processIndex --;
+    if(status == 0){
+        suspendedCommandName[processIndex-1] = NULL;
+        suspendedProcess[processIndex-1] = 0;
+        processIndex --;
+    }else{
+        suspendedCommandName[processIndex-1] = currCommandName;
+        suspendedProcess[processIndex-1] = currProcess;
+    }
     return;
 }
 // BUITDIN EXIT
 void ex(int argNum){
     if(argNum != 0){
-        printf(invalidCommand);
+        fprintf(stderr,invalidCommand);
         return;
     }else if(suspendedProcess[0] != 0){
-        printf(suspendedJobs);
+        fprintf(stderr,suspendedJobs);
         return;
     }
-    raise(SIGTERM);
+    exit(0);
 }
 
 // Reference: inclass notes
@@ -218,7 +228,7 @@ int run(char* path, char ** args,int append, char* input, char* output, int* fd,
         if (input != NULL) {
             int fd1;
             if((fd1=open(input, O_RDONLY)) < 0){
-                printf(invalidFile);
+                fprintf(stderr,invalidFile);
                 exit(-1);
             }
             if (dup2(fd1, STDIN_FILENO) == -1) {
@@ -231,7 +241,7 @@ int run(char* path, char ** args,int append, char* input, char* output, int* fd,
             // Reference: https://stackoverflow.com/questions/23092040/how-to-open-a-file-which-overwrite-existing-content
             fd2 = append == 0 ? open(output, O_WRONLY | O_CREAT | O_TRUNC,0644):open(output, O_WRONLY | O_APPEND | O_CREAT,0644);
             if (fd2< 0) {
-                printf(invalidFile);
+                fprintf(stderr,invalidFile);
                 exit(-1);
             }
             if (dup2(fd2, STDOUT_FILENO) == -1) {
@@ -248,7 +258,7 @@ int run(char* path, char ** args,int append, char* input, char* output, int* fd,
         }
         close(fd[0]); 
         execv(path,args);
-        printf(invalidProgram);
+        fprintf(stderr,invalidProgram);
         exit(-1);
     }
     // Unfortunately if fork fails
@@ -261,6 +271,22 @@ int run(char* path, char ** args,int append, char* input, char* output, int* fd,
         int status; 
         // Get child return, reference: https://www.geeksforgeeks.org/exit-status-child-process-linux/
         waitpid(pid, &status, WUNTRACED);
+        int stopped = WIFSTOPPED(status);
+        if(stopped == 1){
+            suspendedProcess[processIndex] = *currentProcess;
+            int i = 0;
+            char** copyName = (char**) malloc(100*sizeof(char*));
+            while(currCommandName[i] != NULL){
+                char* copyArg = (char*) malloc(100*sizeof(char));
+                strncpy(copyArg,currCommandName[i],100);
+                copyName[i] = copyArg;
+                i++;
+            }
+            copyName[i] = NULL;
+            suspendedCommandName[processIndex] = copyName;
+            processIndex ++;
+            *currentProcess = 0;
+        }
         close(fd[1]);
 		*fdd = fd[0];
         *currentProcess = 0;
@@ -307,14 +333,14 @@ int exec(int argc, char ** argv, char * input, char * output, int append, int* f
 
 // [nyush lab2]$
 // argc: number of arguments
-// argv: an array of arguments
+// argv: an array of arguminvalidsuspents
 void manipulate_args(int argc, char ** argv, int toBreak, pid_t* currentProcess){
     if(toBreak == 1){
         printf("\n");
         return;
     }
     if(isValid(argc,argv)==-1){
-        printf("%s",invalidCommand);
+        fprintf(stderr,invalidCommand);
         return;
     }
     // Pipe
@@ -348,9 +374,14 @@ void manipulate_args(int argc, char ** argv, int toBreak, pid_t* currentProcess)
     }
     // BUILDIN fg
     if(strcmp(argv[0], "fg") == 0){
-        fg(atoi(argv[1]),argc-1);
+        if(argv[1]){
+            fg(atoi(argv[1]),argc-1);
+        }else{
+            fg(666,argc-1);
+        }
         return;
     }
+    // printf("%d", strcmp(argv[0], "exit"));
     // BUILDIN exit
     if(strcmp(argv[0], "exit") == 0){
         ex(argc-1);
@@ -405,7 +436,7 @@ void manipulate_args(int argc, char ** argv, int toBreak, pid_t* currentProcess)
 
 int shell() {
   signal(SIGINT, ctrlC);
-  signal(SIGQUIT, ctrlZ);
+  signal(SIGTSTP, ctrlZ);
   // SHELL
   char input[MAX_INPUT_LENGTH];
   int flag = 0;
@@ -414,7 +445,15 @@ int shell() {
     // Reference: https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
     char cwd[PATH_MAX];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("[nyush %s]$ ", cwd);
+        char dir[100];
+        char * currDir = strtok(cwd, "/"); // Split by space
+        while (currDir != NULL) {
+            // printf(currDir);
+            strncpy(dir,currDir,100);
+            currDir = strtok(NULL, "/");
+        }
+        printf("[nyush %s]$ ", dir);
+        fflush(stdout);
     } else {
         perror("getcwd() error");
         return 1;
@@ -422,7 +461,7 @@ int shell() {
 
     // Read user input
     if (fgets(input, sizeof(input), stdin) == NULL) {
-      continue; 
+      break; 
     }
     
     input[strcspn(input, "\n")] = '\0';
@@ -439,10 +478,10 @@ int shell() {
     }
     token = strtok(input, " "); // Split by space
     while (token != NULL) {
-      currCommandName[num] = token;
-      args[num] = token;
-      token = strtok(NULL, " ");
-      num++;
+        currCommandName[num] = token;
+        args[num] = token;
+        token = strtok(NULL, " ");
+        num++;
     }
     currCommandName[num] = NULL;
     // Call Terminal function to process the arguments
